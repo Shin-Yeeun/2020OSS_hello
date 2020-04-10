68510371b72a29f74912ba0db0010e1a1cb282ef
Shin Yeeun <21900400@handong.edu>
2020-04-10 20:23:44 +0900

    Update

--- hello.c
+++ hello.c
@@ -1,10 +1,6 @@
 #object files
 #include<stdio.h>
 int main(void){
-<<<<<<< HEAD
-	printf("Hello!!!);
+	printf("Hello!!!");
 	return 0;
-=======
-	printf("Hello!!!
->>>>>>> a65dc0655b20e2618d520823cce32837e20e86cf
 }
